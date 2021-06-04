ebeea44c7778024f6a909dd9c5944bb4a9c8128b
Kris Hopper <hopperdavis@gmail.com>
2021-06-04 14:33:11 -0700

    start custom styling to page

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,8 @@
-// const beverage = $("#beverage").val();
-// const flavor = $("input:radio[name=flavor]:checked").val();
-
-// Holy Grail: Critical Error
-
-//Business logic
-
 function userTotal(color, assyria, swallow, knights) {
   return color + assyria + swallow + knights
 }
 
 
-// User Interface Logic
 $(document).ready(function()  {
   $("form#language-selector").submit(function(event){
     event.preventDefault();
@@ -23,16 +15,7 @@ $(document).ready(function()  {
     const total = userTotal(color, assyria, swallow, knights);
     $("#username").text(name);
     $("#username-fail").text(name);
-    
-    
-    // if else statements here
-    // pick what quest directs to certain results
-    // if radio button for holy grail selected - return error, don't get to cross bridge
 
-    
-    // Webdesign 0-25: Ruby 26-50: Java 51-75: C#
-// Desktop 0-25: Java 26-50: C# 51-75: Ruby
-// Mobile 0-25: C# 26-50: Ruby 51-75: Java
     if ((quest === "web-design")) {
       if (total <=25) {
         $("#language").text("Ruby");
@@ -94,8 +77,6 @@ $(document).ready(function()  {
   });
 });
 
-// If they pick Holy Grail it's an immediate fail that should be easy enough.
-// If They pi
 
 
 
