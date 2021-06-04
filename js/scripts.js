6bf0d7d21c8a6e296cbc1ad065a641656c58dbd8
Kris Hopper <hopperdavis@gmail.com>
2021-06-04 10:14:05 -0700

    WIP: working on form questions and thinking about the JS logic needed to get results

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,21 @@
 // const beverage = $("#beverage").val();
 // const flavor = $("input:radio[name=flavor]:checked").val();
+// HTML: 1-5
+// Java: 6-10
+// C#: 11-15
+
+//Business logic
+
+function totalValue(number1, number2, number3, number4) {
+  return number1 + number2 + number3 + number4
+}
+
+
+// User Interface Logic
+
+
+
+
+
+// if else statements here
+// pick what quest directs to certain results
