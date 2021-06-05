ab92a81dc019ff219c83b0fbd9dadf2a1826c132
Kris Hopper <hopperdavis@gmail.com>
2021-06-04 17:02:17 -0700

    add README and final submit of project

---
+++ README.md
@@ -0,0 +1,38 @@
+# Programming Language Selector
+
+#### A JavaScript interface for determining a user's preferred Programming Language
+
+#### By Kristen Hopper
+
+## Technologies Used
+
+* _HTML_
+* _CSS_
+* _JavaScript_
+* _jQUery_
+
+## Description
+
+This form uses JavaScript and jQuery to help a user determine which programming language they should learn first. 
+
+## Setup/Installation Requirements
+
+1. Create an empty directory with no local repository. 
+2. Within the new directory, run `git clone https://github.com/krishops/language-selector.git` in the terminal to clone the project files to a local repository.
+2. Open the project within a code editor, such as Visual Studio Code, to make changes to the project. 
+
+* [Click here](https://krishops.github.io/language-selector) to access a live version of the project.
+
+## Known Bugs
+
+* There are no known bugs in this project
+
+## License
+
+[MIT](https://opensource.org/licenses/MIT)
+
+Copyright (c) Kristen Hopper
+
+## Contact Information
+
+hopperdavis@gmail.com
--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,8 @@ function userTotal(color, assyria, swallow, knights) {
   return color + assyria + swallow + knights
 }
 
-
-
-
-$(document).ready(function()  {
-  $("form#language-selector").submit(function(event){
+$(document).ready(function () {
+  $("form#language-selector").submit(function (event) {
     event.preventDefault();
     const name = $("#name").val();
     const quest = $("input:radio[name=quest]:checked").val();
@@ -18,62 +15,53 @@ $(document).ready(function()  {
     $("#username").text(name);
     $("#username-fail").text(name);
 
-    $(".btn-danger").click(function(){
+    $(".btn-danger").click(function () {
       $("#language-selector").trigger("reset");
       $("#hidden").hide();
       $("#hidden-fail").hide()
     });
-  
-
-    console.log(name);
-    console.log(quest)
-    console.log(color);
-    console.log(assyria);
-    console.log(swallow);
-    console.log(knights);
-    console.log(total);
 
     if ((quest === "web-design")) {
-      if (total <=25) {
+      if (total <= 25) {
         $("#language").text("Ruby");
         $("#hidden").show();
-      } else if (total >26 && total <=50) {
+      } else if (total > 26 && total <= 50) {
         $("#language").text("Java");
         $("#hidden").show();
-      } else if (total >51 && total <=75) {
+      } else if (total > 51 && total <= 75) {
         $("#language").text("C#");
         $("#hidden").show();
       }
     }
 
     if ((quest === "desktop")) {
-      if (total <=25) {
+      if (total <= 25) {
         $("#language").text("C#");
         $("#hidden").show();
-      } else if (total >26 && total <=50) {
+      } else if (total > 26 && total <= 50) {
         $("#language").text("Ruby");
         $("#hidden").show();
-      } else if (total >51 && total <=75) {
+      } else if (total > 51 && total <= 75) {
         $("#language").text("Java");
         $("#hidden").show();
       }
     }
-    
+
     if ((quest === "mobile")) {
-      if (total <=25) {
+      if (total <= 25) {
         $("#language").text("Java");
         $("#hidden").show();
-      } else if (total >26 && total <=50) {
+      } else if (total > 26 && total <= 50) {
         $("#language").text("C#");
         $("#hidden").show();
-      } else if (total >51 && total <=75) {
+      } else if (total > 51 && total <= 75) {
         $("#language").text("Ruby");
         $("#hidden").show();
       }
     }
 
     if ((quest === "holy-grail")) {
-      if (total >=0 && total <=75)  {
+      if (total >= 0 && total <= 75) {
         $("#hidden-fail").show();
       }
     }
